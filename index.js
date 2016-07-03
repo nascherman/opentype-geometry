16515d25c56eb74f715860de102ae5be3db56ca9
nascherman <n_scherman@hotmail.com>
2016-07-03 12:33:18 -0400

    class object

--- index.js
+++ index.js
@@ -2,12 +2,42 @@ const opentype = require('opentype.js');
 const computeLayout = require('opentype-layout');
 const pathToShape = require('./util/pathToShape');
 const defaults = require('lodash.defaults');
+const xtend = require('xtend');
 const THREE = require('three');
 
-module.exports = function createText(opts) {
+function noop() {}
 
+function OpenTypeGeometry(opts) {
+  opts = opts || {};
+  if(opts.fontFace) 
+    this.loadText(opts);
+}
+
+OpenTypeGeometry.prototype.setText = function(text) {
+  let _this = this;
+    let scale = 1 / this.font.unitsPerEm * this.fontSizePx;
+    let result = computeLayout(this.font, text, {
+      lineHeight: this.lineHeight * this.font.unitsPerEm,
+      width: this.width / scale
+    });
+
+    this.currentText = result.glyphs.map(function(glyph) {
+      return {
+        position: glyph.position,
+        row: glyph.row,
+        index: glyph.index,
+        geometry:_this.chars[_this.chars.findIndex(x => x.code === glyph.data.unicode)]
+      };
+    });
+}
+
+OpenTypeGeometry.prototype.loadText = function(opts) {
+  let _this = this;
+  if(!opts.fontFace) throw new Error('must specify a fontface to load opentype geometry');
   defaults(opts, {
-    fontSize: 72,
+    fontSizePx: 72,
+    lineHeight: 2,
+    width: 600,
     text: 'No text',
     extrude: {
       amount: 1000,
@@ -17,30 +47,27 @@ module.exports = function createText(opts) {
       bevelSize       : 100,
       bevelSegments   : 4
     },
-    fontFace: './demo/fonts/Pacifico.ttf'
+    callback: noop
   });
 
-  opentype.load(opts.fontFace, (err, font) => {
+  Object.assign(this, opts);
+
+  opentype.load(opts.fontFace, function(err, font) {
     if (err) throw err;
-  
-    let fontSizePx = 72;
-    let text = 'Hello World! This box should start word-wrapping!\n' +
-      'Symbols !@#$%^&*()__+{}'
-    let scale = 1 / font.unitsPerEm * fontSizePx;
-    // Layout some text - notice everything is in em units!
-    let result = computeLayout(font, text, {
-      lineHeight: 2 * font.unitsPerEm, // '2.5em' in font units
-      width: 600 / scale // '500px' in font units
+    _this.chars = [];
+    Object.keys(font.glyphs.glyphs).forEach(function(glyphKey) {
+      let glyph = font.glyphs.glyphs[glyphKey];
+      if(glyph.unicode !== undefined && typeof String.fromCharCode(glyph.unicode) === 'string') {
+        _this.chars.push({
+          code: glyph.unicode,
+          geometry: pathToShape(glyph.path, opts, THREE)
+        });
+      }
     });
-    let glyphGeometry = result.glyphs.map((glyph) => {
-      return {
-        position: glyph.position,
-        row: glyph.row,
-        index: glyph.index,
-        geometry: pathToShape(glyph.data.path, opts, THREE)
-      };
-    });
-    
-    return glyphGeometry;
+
+    _this.font = font;
+    opts.callback();
   });
 }
+
+module.exports = OpenTypeGeometry
