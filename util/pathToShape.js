843546b3fcc1a4334c3a129f1346c676efbbb353
nascherman <n_scherman@hotmail.com>
2016-07-05 18:21:41 -0400

    pathing check for shapes

--- util/pathToShape.js
+++ util/pathToShape.js
@@ -7,13 +7,6 @@ var parse = require('parse-svg-path')
 var contours = require('svg-path-contours')
 
 function svgPathsToShape (path, opts, THREE) {
-  opts = assign({
-    delaunay: true,
-    clean: true,
-    exterior: false,
-    scale: 1
-  }, opts);
-
   // a little extrusion by default
   let extrudeSettings = defaults(opts.extrude, {
     amount: 20,
@@ -38,16 +31,19 @@ function svgPathsToShape (path, opts, THREE) {
     if(path.length === 0) return;
     let shapePath = transformSVGPath(path, THREE);
     if(!(shapePath.actions.length === 1 && shapePath.actions.args === undefined)) {
-      shapes.push( shapePath);
-      shapeGeo.push(new THREE.ShapeGeometry(shapes[i-1] ) );      
+      shapePath.index = i;
+      shapes.push(shapePath);
+      shapeGeo.push(new THREE.ShapeGeometry(shapes[i-1]));
     }
   });
 
   shapeGeo.forEach((geo) => {
     geo.computeBoundingBox();
+    geo.computeBoundingSphere();
   });
 
   let foundHole;
+  let merged = [];
   shapes.forEach((shape, i) => {
     let bounds = shapeGeo[i].boundingBox;
     shapeGeo.forEach((geo, j) => {
@@ -55,22 +51,36 @@ function svgPathsToShape (path, opts, THREE) {
       if(bounds.max.x > geo.boundingBox.max.x && bounds.max.y > geo.boundingBox.max.y &&
         bounds.min.x < geo.boundingBox.min.x && bounds.min.y < geo.boundingBox.min.y) {
         shape.holes.push(shapes[j]);
+        merged.push(shapes[j])
+        merged.push(shapes[i])
       }
-
     });
 
     if(shape.holes.length > 0) {
       foundHole = true;
-      //let newShape = new THREE.ShapeGeometry(shape);
       let newShape = new THREE.ExtrudeGeometry(shape, extrudeSettings);
       geo.merge(newShape);
     }
+    
   });
   if(!foundHole) {
     shapes.forEach((shape) => { 
       let newShape = new THREE.ExtrudeGeometry(shape, extrudeSettings);
-      geo.merge(newShape); 
+      geo.merge(newShape);
     });
+  }
+  else {
+    shapes.forEach((shape) => {
+      let foundShape = false;
+      merged.forEach((mergedShape) => {
+        if(mergedShape.index === shape.index) 
+          foundShape = true;
+      });
+      if(foundShape === false) {
+        let newShape = new THREE.ExtrudeGeometry(shape, extrudeSettings);
+        geo.merge(newShape);
+      }
+    })
   }
 
   return geo;
